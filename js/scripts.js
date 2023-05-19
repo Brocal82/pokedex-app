01eb71bf4a03a70b62490327514d2f6a9463ab25
Brocal82 <antonioruizbrocal@gmail.com>
2023-05-19 19:10:07 +0200

    Updated Js file task 1.2
     If applied, this commit will add the First Array Pokemon project and many trials with console.log

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-alert('Hola World!')
+let pokemonList = [
+    {name:'Pikachu', heights:0.4, type: ['electric']},
+    {name:'Spearow', heights:0.3, type:['flying', 'normal']},
+    {name:'Charizard', heights:1.7, type:['fire', 'flying']},
+    {name:'Mr.Mime', heights:1.3, type:['Psychic', 'Fairy']},
+    {name:'Golbat', heights:1.6, type:['poison', 'flying']}
+];
 
-let favoriteFood = 'Paella';
-document.write(favoriteFood);
+// console.log(pokemonList.length);
+// console.log(typeof pokemonList);
+// console.log(pokemonList[0].name);
+// console.log(pokemonList[2].type[0]);
+// console.log(pokemonList[4].type);
+// console.log(pokemonList[3].name);
+
+// pokemonList[0].name = pokemonList[4].name;
+// console.log(pokemonList[0].name);
+
+// pokemonList[2].heights = pokemonList[0].heights;
+// console.log(pokemonList[2].heights); 
+
+// pokemonList[3].type[0] = pokemonList[4].type[0];
+// console.log(pokemonList[3].type[0]);
