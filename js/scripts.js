8c06a6c3ae8e8c77937ab19b1fd556e8536ad8c6
Brocal82 <antonioruizbrocal@gmail.com>
2023-05-28 10:46:13 +0200

    forEach in IIFE
    If applied this commit will add the forEach function inside the IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-let pokemonList = [
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+
     {name:'Pikachu', heights:0.4, type: ['electric']},
     {name:'Spearow', heights:0.3, type:['flying', 'normal']},
     {name:'Charizard', heights:1.7, type:['fire', 'flying']},
@@ -7,27 +10,57 @@ let pokemonList = [
 ];
 
 
+function getAll() {
+    return pokemonList;
+}
+
+
+function add (pokemon) {
+    pokemonList.push(pokemon);
+}
+
+
 pokemonList.forEach (function (printArrayDetails) {
+    
+    
+    if (printArrayDetails.heights < 1 && printArrayDetails.heights > 0.3) {
+        
+        document.write(printArrayDetails.name + " is a small Pokemon " + printArrayDetails.heights + " (heights)" + '<br>');
+        
+        
+    }else if (printArrayDetails.heights < 1.5 && printArrayDetails.heights > 1) {
+        
+        document.write(printArrayDetails.name + " is a medium Pokemon " + printArrayDetails.heights + " (heights)"+ '<br>');
+        
+    }else if (printArrayDetails.heights >= 1.7) {
+        
+        document.write(printArrayDetails.name + " is a big Pokemon " + printArrayDetails.heights + " (heights)"+ " WOW This one is a big one!!!!" + '<br>');
+        
+    }else {
+        
+        document.write(printArrayDetails.name + " is a big Pokemon " + printArrayDetails.heights + " (heights)" + '<br>');
+    }
+});
 
 
-        if (printArrayDetails.heights < 1 && printArrayDetails.heights > 0.3) {
-    
-            document.write(printArrayDetails.name + " is a small Pokemon " + printArrayDetails.heights + " (heights)" + '<br>');
-    
-    
-        }else if (printArrayDetails.heights < 1.5 && printArrayDetails.heights > 1) {
-            
-            document.write(printArrayDetails.name + " is a medium Pokemon " + printArrayDetails.heights + " (heights)"+ '<br>');
-    
-        }else if (printArrayDetails.heights >= 1.7) {
-    
-            document.write(printArrayDetails.name + " is a big Pokemon " + printArrayDetails.heights + " (heights)"+ " WOW This one is a big one!!!!" + '<br>');
-    
-        }else {
-    
-            document.write(printArrayDetails.name + " is a big Pokemon " + printArrayDetails.heights + " (heights)" + '<br>');
-        }
-        }); 
+return {
+    add: add,
+    getAll: getAll
+}
+
+
+}) ();
+
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Antonio'});
+console.log(pokemonRepository.getAll());
+
+
+
+
+
+
+
 
 // function printArrayDetails() {
 
