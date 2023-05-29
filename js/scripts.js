2245ff9f202b64bbbd020bdf7f823cf7cc2c00f5
Brocal82 <antonioruizbrocal@gmail.com>
2023-05-29 09:40:36 +0200

    Small updates
    If applied this commit  will add the pokemon type in conditionals and delete the unnecesary comments

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,10 @@ let pokemonRepository = (function () {
     let pokemonList = [
 
     {name:'Pikachu', heights:0.4, type: ['electric']},
-    {name:'Spearow', heights:0.3, type:['flying', 'normal']},
-    {name:'Charizard', heights:1.7, type:['fire', 'flying']},
-    {name:'Mr.Mime', heights:1.3, type:['Psychic', 'Fairy']},
-    {name:'Golbat', heights:1.6, type:['poison', 'flying']}
+    {name:'Spearow', heights:0.3, type: ['flying', 'normal']},
+    {name:'Charizard', heights:1.7, type: ['fire', 'flying']},
+    {name:'Mr.Mime', heights:1.3, type: ['Psychic', 'Fairy']},
+    {name:'Golbat', heights:1.6, type: ['poison', 'flying']}
 ];
 
 
@@ -35,20 +35,20 @@ function listPokemon(pokemon) {
     
     if (pokemon.heights < 1 && pokemon.heights > 0.3) {
         
-        document.write(pokemon.name + " is a small Pokemon " + pokemon.heights + " (heights)" + '<br>');
+        document.write(pokemon.name + " is a small Pokemon " + pokemon.heights + " (heights) " + "type: " + pokemon.type + '<br>');
         
         
     }else if (pokemon.heights < 1.5 && pokemon.heights > 1) {
         
-        document.write(pokemon.name + " is a medium Pokemon " + pokemon.heights + " (heights)"+ '<br>');
+        document.write(pokemon.name + " is a medium Pokemon " + pokemon.heights + " (heights) "+ "type: " + pokemon.type + '<br>');
         
     }else if (pokemon.heights >= 1.7) {
         
-        document.write(pokemon.name + " is a big Pokemon " + pokemon.heights + " (heights)"+ " WOW This one is a big one!!!!" + '<br>');
+        document.write(pokemon.name + " is a big Pokemon " + pokemon.heights + " (heights) "+ "type: " + pokemon.type + " WOW This one is a big one!!!!" + '<br>');
         
     }else {
         
-        document.write(pokemon.name + " is a big Pokemon " + pokemon.heights + " (heights)" + '<br>');
+        document.write(pokemon.name + " is a big Pokemon " + pokemon.heights + " (heights) " + "type: " + pokemon.type + '<br>');
     }
 };
 
@@ -57,53 +57,3 @@ console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: 'Quilava', heights: 1.3, type: 'fire'});
 console.log(pokemonRepository.getAll().forEach(listPokemon));
 
-
-
-
-
-
-
-
-
-
-
-
-// function pokemon() {
-
-//     for (let i = 0; i < pokemonList.length; i++) {
-
-//         if (pokemon.heights < 1 && pokemon.heights > 0.3) {
-    
-//             document.write(pokemon.name + " is a small Pokemon " + pokemon.heights + " (heights)" + '<br>');
-    
-    
-//         }else if (pokemon.heights < 1.5 && pokemon.heights > 1) {
-            
-//             document.write(pokemon.name + " is a medium Pokemon " + pokemon.heights + " (heights)"+ '<br>');
-    
-//         }else if (pokemon.heights >= 1.7) {
-    
-//             document.write(pokemon.name + " is a big Pokemon " + pokemon.heights + " (heights)"+ " WOW This one is a big one!!!!" + '<br>');
-    
-//         }else {
-    
-//             document.write(pokemon.name + " is a big Pokemon " + pokemon.heights + " (heights)" + '<br>');
-//         }
-//         }  
-// }
-
-// //The Array is printed as many times as the function is called
-// pokemon(); 
-// pokemon();
-// pokemon();
-
-//----------------------------------------------------------------------------------------------------------------------------
-
-//forEach function:
-
-// pokemonList.forEach(function(pokemon) {
-//     console.log(pokemon);
-// });
-
-
-// pokemon();
